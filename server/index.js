6bd3f2afb8ebe7acb406d36371cb44eaca7328c0
Edanur <edaaoneerr@gmail.com>
2025-12-23 17:36:00 +0000

    version 1.0.15

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const app = express();
 /* -------------------- */
 /* SHOPIFY WEBHOOK */
 /* -------------------- */
+app.use((req, res, next) => {
+  console.log("🔥 INCOMING:", req.method, req.url);
+  next();
+});
 
 app.use("/webhooks/orders-paid", express.json());
 
