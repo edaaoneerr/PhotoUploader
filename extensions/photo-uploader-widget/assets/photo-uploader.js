a1da801ac48e644f0f631617a925b18d741391c8
Edanur <edaaoneerr@gmail.com>
2025-12-24 01:38:13 +0000

    version 1.0.29

--- extensions/photo-uploader-widget/assets/photo-uploader.js
+++ extensions/photo-uploader-widget/assets/photo-uploader.js
@@ -943,7 +943,7 @@ if (!window.__PHOTO_UPLOADER_ADD_HANDLER__) {
         return;
       }
 
-      //window.location.href = '/cart';
+      window.location.href = '/cart';
     } catch (err) {
       console.error('Add to cart network error', err);
       alert('Bir hata oluştu, lütfen tekrar deneyin.');
