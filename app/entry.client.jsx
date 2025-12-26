b5353a5be86e908e8f694855d22fbfc6580674e5
Edanur <edaaoneerr@gmail.com>
2025-12-26 18:18:22 +0000

    version 1.0.73

--- app/entry.client.jsx
+++
@@ -1,8 +0,0 @@
-import { hydrateRoot } from "react-dom/client";
-import { RouterProvider } from "@shopify/shopify-app-react-router/react";
-import routes from "@shopify/shopify-app-react-router/routes";
-
-hydrateRoot(
-  document,
-  <RouterProvider routes={routes} />
-);
