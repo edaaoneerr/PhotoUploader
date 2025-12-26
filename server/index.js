941d1e941aab2e7b94c5655818484a25721f0229
Edanur <edaaoneerr@gmail.com>
2025-12-26 21:02:14 +0000

    version 1.0.77

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import fetch from "node-fetch";
 import { createRequestHandler } from "@react-router/express";
-import * as build from "../build/server/index.js";
 
 const WORKER_BASE = "https://magnet-upload.kendinehasyazilimci.workers.dev";
 const prisma = new PrismaClient();
